0d879849c8a2655f15804c4cfb4b9feb0d8d08ec
Wa Ode Dewi <waodedewi@Was-MacBook-Air.local>
2026-04-24 23:58:21 +0700

    Tugas Aplikasi Tahun Kabisat semester 2

---
+++ src/ERP/main.java
@@ -0,0 +1,28 @@
+package ERP;
+
+import javafx.application.Application;
+//import javafx.scene.Scene;
+//import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+//import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.stage.Stage;
+
+public class main extends Application{
+    
+    @Override
+
+    public void start(Stage stage){
+
+        Label title = new Label("PT. INDONESIA EMAS");
+        title.setFont(new Font("Times New Roman", 26));
+        VBox root = new VBox();
+        root.setAlignment(Pos.BASELINE_LEFT);
+
+        
+
+        
+    }
+}
